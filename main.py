513c3d60d8b59594ef8883feb0000c53c6e2b39e
Zach <zach@box.zzimm.com>
2022-02-20 21:07:49 -0800

    Lost commit

--- main.py
+++ main.py
@@ -5,20 +5,21 @@ from flask_cors import CORS, cross_origin
 from flask_restful import Api, Resource, reqparse
 import time
 import pyfirmata
+
 # from api.HelloApiHandler import HelloApiHandler
 
 app = Flask(__name__, static_url_path='',static_folder='frontend/build')
 
-CORS(app, resources={r'/api/*': {"origins": "*"}})
-# CORS(app, 
-#     origins=["*"],
-#     methods=["OPTIONS", "GET", "POST"],
-#     allow_headers=["Content-Type"])
+# CORS(app, resources={r'/pulse/*': {"origins": "*"}})
+CORS(app, 
+    origins=["*"],
+    methods=["OPTIONS", "GET", "POST"],
+    allow_headers=["Content-Type"])
 #app.config['CORS_HEADERS'] = 'Content-Type'
 # api = Api(app)
 logging.basicConfig(level=logging.DEBUG)
 logging.getLogger('flask-cors').level = logging.DEBUG
-# board = pyfirmata.Arduino('/dev/ttyACM0')
+board = pyfirmata.Arduino('/dev/ttyACM0')
 # api.add_resource(HelloApiHandler, '/flask/hello')
 
 @app.route("/api/",defaults={'path': ''},methods=["POST","GET"])
@@ -30,12 +31,30 @@ def serve(path):
     data = {"name":"Sean"}
     if(request.method == "GET"): return data
     return render_template(app.static_folder + '/index.html', data=data)
+
+@app.route("/post/",defaults={'path':''}, methods=["GET","POST"])
+@app.route("/post/<path:path>",methods=["POST","GET"])    
+@cross_origin(origins=["*"])
+def post(path):
+    content = request.json['name']
+    print(content)
+    return request.json
     
-def pulse():
-    print("Pulse!")
-    # board.digital[12].write(1)
-    # time.sleep(1)
-    # board.digital[12].write(0)
+@app.route("/pulse/",defaults={'path':''}, methods=["GET", "POST"])
+@app.route("/pulse/<path:path>",methods=["POST","GET"])    
+@cross_origin(origins=["*"])
+def pulse(path):
+    print(".pulse")
+    if(request.method == "POST"):
+        print("Pulse!")
+        board.digital[12].write(1)
+        time.sleep(1)
+        board.digital[12].write(0)
+
+    return {"name":"Pulse!"}
+
+
+
 
 if __name__ == "__main__":
     import host_meta
